7c5e328e777221252180e3a0b08b7606a7f92f16
Mamadou Mbodj <mbodjmamadou1999@gmail.com>
2026-01-23 19:20:33 +0000

    initial dict

--- cleansurvey/1_data_exploration/0_setup.R
+++ cleansurvey/1_data_exploration/0_setup.R
@@ -1,12 +1,3 @@
-pacman::p_load(
-  haven
-)
 
-# Read data from data folder
-df <- read_dta(
-  file.path("data/base_ehcvm2.dta")
-)
-
-str(df)
 
 
---
+++ cleansurvey/config.R
@@ -0,0 +1,4 @@
+# This file is to do basic configuration
+MAIN_DATA_PATH <- "data/base_ehcvm2.dta"
+
+AUX_FILE_PATH <- "data/aux_file"
